be8a60bfe5d2ab28fa24278a14f198500e843a95
davidmpon <botuchiwa@gmail.com>
2024-09-27 16:53:30 +0100

    Update Salut.js

--- scripts/cmds/Salut.js
+++ scripts/cmds/Salut.js
@@ -1,20 +1,20 @@
 module.exports = {
-	config: {
-			name: "salut",
-			version: "1.0",
-			author: "Jaychris Garcia",
-			countDown: 5,
-			role: 0,
-			shortDescription: "sarcasm",
-			longDescription: "sarcasm",
-			category: "reply",
-	},
+    config: {
+        name: "salut",
+        version: "1.0",
+        author: "Jaychris Garcia",
+        countDown: 5,
+        role: 0,
+        shortDescription: "sarcasm",
+        longDescription: "sarcasm",
+        category: "reply",
+    },
 onStart: async function(){}, 
 onChat: async function({
-	event,
-	message,
-	getLang
+    event,
+    message,
+    getLang
 }) {
-	if (event.body && event.body.toLowerCase() == "hi") return message.reply("Yo comment tu vas et n'oublie pas que Shibai Otsutsuki c'est le meilleur ");
+    if (event.body && event.body.toLowerCase() == "salut") return message.reply("𝗦𝗮𝗹𝘂𝘁 𝗯𝗿𝗼 🤵 , 𝒄𝒐𝒎𝒎𝒆𝒏𝒕 𝒕𝒖 𝒗𝒂𝒔? 𝗺𝗼𝗶 𝗰'𝗲𝘀𝘁 David Bot 👨‍💻 𝒒𝒖'𝒆𝒔 𝒄𝒆 𝒒𝒖𝒆 𝒋𝒆 𝒑𝒆𝒖𝒙 𝒇𝒂𝒊𝒓𝒆 𝒑𝒐𝒖𝒓 𝒕𝒐𝒊 𝒕𝒂𝒑𝒆   +help ou +Aide pour voir ma liste 𝕕𝕖𝕤 𝕔𝕠𝕞𝕞𝕒𝕟𝕕𝕖s")
 }
 };

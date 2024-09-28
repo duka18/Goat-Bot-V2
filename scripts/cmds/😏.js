89c3554a6289945799e5434f1b33854cd962df27
davidmpon <botuchiwa@gmail.com>
2024-09-28 13:33:12 +0100

    Create 😏.js

---
+++ scripts/cmds/😏.js
@@ -0,0 +1,20 @@
+module.exports = {
+    config: {
+        name: "😏",
+        version: "1.0",
+        author: "𝐦𝐚𝐝𝐚𝐫𝐚",
+        countDown: 5,
+        role: 0,
+        shortDescription: "commande 😏",
+        longDescription: "commande O😏k",
+        category: "reply",
+    },
+    onStart: async function(){}, 
+    onChat: async function({
+        event,
+        message,
+        getLang
+    }) {
+        if (event.body && event.body.toLowerCase() == "😏") return message.reply("félicitations__◥✇◣, ,◢✇◤__ frère");
+    }
+}

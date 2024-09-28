2ad0040aea88ff8cccd7cd913782a6c55c7d1b55
davidmpon <botuchiwa@gmail.com>
2024-09-28 11:41:45 +0100

    Create ☝️.js

---
+++ scripts/cmds/☝️.js
@@ -0,0 +1,20 @@
+module.exports = {
+    config: {
+        name: "☝️",
+        version: "1.0",
+        author: "𝐦𝐚𝐝𝐚𝐫𝐚",
+        countDown: 5,
+        role: 0,
+        shortDescription: "commande ☝️",
+        longDescription: "commande O☝️k",
+        category: "reply",
+    },
+    onStart: async function(){}, 
+    onChat: async function({
+        event,
+        message,
+        getLang
+    }) {
+        if (event.body && event.body.toLowerCase() == "☝️") return message.reply("David mpongo _________◥✇◣, ,◢✇◤_________ce lui le meilleur_");
+    }
+}

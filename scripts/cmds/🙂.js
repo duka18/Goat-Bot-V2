a116a7d9733fd03e2d7ce58e0c126dcf4a88b1fd
davidmpon <botuchiwa@gmail.com>
2024-09-28 10:34:13 +0100

    Create 🙂.js

---
+++ scripts/cmds/🙂.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+module.exports = {
+  config: {
+    name: "ns",
+    version: "1.0",
+    author: "Kennethhttps://pastebin.com/vLS9hSye",
+    countDown: 5,
+    role: 0,
+    shortDescription: "o🟢k",
+    longDescription: "auto bot reply to your message",
+    category: "no prefix",
+  },
+ 
+  onStart: async function() {},
+ 
+  onChat: async function({ event, message, getLang, api }) {
+    if (event.body) {
+      const word = event.body.toLowerCase();
+      switch (word) {
+        case "🙂":
+          const replies = [
+            " Tu es aussi bon que mon maître David mpongo l'aigle royal",
+          ];
+          api.setMessageReaction("🥰", event.messageID, event.messageID, api); 
+          const randomIndex = Math.floor(Math.random() * replies.length);
+          message.reply({
+            body: replies[randomIndex],
+          });
+          break;
+        default:
+          return; 
+      }
+    }
+  },
+};

93f281086f2b8f9f634ca86d688b5b6802fd8d09
davidmpon <botuchiwa@gmail.com>
2024-10-03 17:42:27 +0100

    Update prefix.js

--- scripts/cmds/prefix.js
+++ scripts/cmds/prefix.js
@@ -45,7 +45,7 @@ module.exports = {
 			confirmThisThread: "Please react to this message to confirm change prefix in your box chat",
 			successGlobal: "Changed prefix of system bot to: %1",
 			successThisThread: "Changed prefix in your box chat to: %1",
-			myPrefix: "🌐 Voici le Prefix que ma David mpongo%2"
+			myPrefix: "🌐 Voici le Prefix que ma donné David mpongo%2"
 		}
 	},
 

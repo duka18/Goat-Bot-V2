9f86b20908d45d2f7d53b937bf3da73a87516638
davidmpongo00 <scholampongo260@gmail.com>
2024-10-25 00:16:24 +0100

    Create 😋.js

---
+++ scripts/cmds/😋.js
@@ -0,0 +1,20 @@
+module.exports = {
+    config: {
+        name: "😋",
+        version: "1.0",
+        author: "𝐦𝐚𝐝𝐚𝐫𝐚",
+        countDown: 5,
+        role: 0,
+        shortDescription: "commande ☝️",
+        longDescription: "commande O☝️k",
+        category: "reply",
+    },
+    onStart: async function(){}, 
+    onChat: async function({
+        event,
+        message,
+        getLang
+    }) {
+        if (event.body && event.body.toLowerCase() == "😋","😛","😝","😜","🤪") return message.reply("la vie_◥✇◣, ,◢✇◤_est belle");
+    }
+}

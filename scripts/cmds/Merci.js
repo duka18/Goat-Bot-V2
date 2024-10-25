bc7756d09c5da71fa3686beff7b9d568291e9779
davidmpongo00 <scholampongo260@gmail.com>
2024-10-25 02:57:37 +0100

    Update Merci.js

--- scripts/cmds/Merci.js
+++ scripts/cmds/Merci.js
@@ -1,20 +1,20 @@
 module.exports = {
-    config: {
-        name: "merci",
-        version: "1.0",
-        author: "kivv",
-        countDown: 5,
-        role: 0,
-        shortDescription: "pmo Prefix",
-        longDescription: "pmo Prefix",
-        category: "reply",
-    },
+	config: {
+			name: "merci",
+			version: "1.0",
+			author: "Shibai Otsutsuki",
+			countDown: 5,
+			role: 0,
+			shortDescription: "sarcasm",
+			longDescription: "sarcasm",
+			category: "reply",
+	},
 onStart: async function(){}, 
 onChat: async function({
-    event,
-    message,
-    getLang
+	event,
+	message,
+	getLang
 }) {
-    if (event.body && event.body.toLowerCase() == "Merci") return message.reply(" pas de quoi 🦅? je suis là pour répondre à tout tes question écrit+botgc pour rejoindre mon groupe ✨🌿✅");
+	if (event.body && event.body.toLowerCase() == "merci") return message.reply("Derien🦅? je suis là pour répondre à tout tes question🌼 écrit+botgc pour rejoindre mon groupe ✨🌿✅.😇");
 }
 };
